f564dd195280845eb441a1ba43a4d343a13b751f
Tritonhalley <tritonh@pop-os.localdomain>
2022-07-10 12:01:40 +0430

    define Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
-
+import { Route, Routes, Navigate } from "react-router-dom";
+import React from "react";
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" />
+        <Route path="/about-us" />
+        <Route path="/faq" />
+        <Route path="/contact-us" />
+        <Route path="/Survey" />
+        <Route path="*" />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
