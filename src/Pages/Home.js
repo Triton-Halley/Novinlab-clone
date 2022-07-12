d8c4f496823c14b175a537fb399596e2640c97dd
Tritonhalley <tritonh@pop-os.localdomain>
2022-07-12 10:47:50 +0430

    Add Lab Sections

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import HeroSlider from "../Components/Hero/HeroSilder";
 import BoxContent from "../Components/Hero/HeroContentBox/Boxs";
 import LabServices from "../Components/ServicesBox/LabServices";
+import LabSection from "../Components/LabSection/LabSection";
 const Home = () => {
   return (
     <>
       <HeroSlider />
       <BoxContent />
       <LabServices />
+      <LabSection />
     </>
   );
 };
