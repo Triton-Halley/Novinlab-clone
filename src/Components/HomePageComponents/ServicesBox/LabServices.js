9c561cb727375a949aa7910ca6aebd5ce2e4e7db
Ali Geshani <Triton.halley@outlook.com>
2022-08-02 19:33:58 +0430

    change imports path , change header text , change whole style

--- src/Components/HomePageComponents/ServicesBox/LabServices.js
+++ src/Components/HomePageComponents/ServicesBox/LabServices.js
@@ -1,54 +1,54 @@
 import ServiceBox from "./ServiceBox";
 import classes from "./LabServices.module.css";
-import ChemicalIcon from "../../../assets/icons/chemical.png";
-import BabyIcon from "../../../assets/icons/baby.png";
-import BloodIcon from "../../../assets/icons/blood.png";
-import ChemicalTestIcon from "../../../assets/icons/chemicalTest.png";
-import MicroscopeIcon from "../../../assets/icons/microscope.png";
-import ReproductionIcon from "../../../assets/icons/reproduction.png";
+import urea from "../../../assets/icons/blood-urea-nitrogen.png";
+import BabyIcon from "../../../assets/icons/newborn.png";
+import bloodTube from "../../../assets/icons/test-tube.png";
+import Sweat from "../../../assets/icons/sweat.png";
+import ColposCope from "../../../assets/icons/colposcope.png";
+import IUI from "../../../assets/icons/ivf.png";
 
 const DUMMY_DATA = [
   {
     id: "d1",
     title: "تست UBT - UST یا اوره آز",
     description:
-      "این تست با توجه به سرعت و دقت خوبی که دارد برای افراد مشکوک به مشکلات معده و برای سنجش پایه توصیه می شود.",
-    imgSrc: ChemicalIcon,
+      "این تست با توجه به سرعت و دقت خوبی که دارد برای افراد مشکوک به مشکلات معده و ...",
+    imgSrc: urea,
   },
   {
     id: "d2",
     title: "تست بیلی روبین",
     description:
-      "تست بیلی روبین یا زردی نوزادان با دستگاه از طریق  اندازه گیری زردی پوست بدون نیاز به خون گیری یا حتی لمس نوزاد صورت می پذیرد",
+      "تست بیلی روبین یا زردی نوزادان با دستگاه از طریق  اندازه گیری زردی پوست بدون نیاز به ...",
     imgSrc: BabyIcon,
   },
   {
     id: "d3",
     title: "ونوجکت",
     description:
-      "آزمایشگاه نوین برای کاهش درد ، راحتی و بهبود نمونه گیری افراد مراجعه کننده از وسایل ونوجکت جهت خونگیری افراد استفاده می نماید.",
-    imgSrc: BloodIcon,
+      "آزمایشگاه نوین برای کاهش درد ، راحتی و بهبود نمونه گیری افراد مراجعه کننده از وسایل ...",
+    imgSrc: bloodTube,
   },
   {
     id: "d4",
     title: "تست عرق",
     description:
-      "کسانی که مشکوک به مشکلات گوارشی مانند سو تغذیه یا عفونت های مکرر مجاری تنفسی یا عرق شور هستند با سنجش مقدار سدیم و کلر موجود در عرق آنها میتوان به جواب اولیه دست پیدا کرد.",
-    imgSrc: ChemicalTestIcon,
+      "کسانی که مشکوک به مشکلات گوارشی مانند سو تغذیه یا عفونت های مکرر مجاری تنفسی یا  ...",
+    imgSrc: Sweat,
   },
   {
     id: "d5",
     title: "کولپوسکوپی بایوپسی",
     description:
-      "انجام کولپوسکوپی برای افرادی که نیاز به بررسی بیشتر در مشکلات زنان دارند.",
-    imgSrc: MicroscopeIcon,
+      "انجام کولپوسکوپی برای افرادی که نیاز به بررسی بیشتر در مشکلات زنان دارند ...",
+    imgSrc: ColposCope,
   },
   {
     id: "d6",
     title: "روش IUI",
     description:
-      "انجام روش IUI برای افرادی که مشکلات نازائی دارند یا اینکه می خواهند جنسیت فرزندشان را خودشان انتخاب کنند.",
-    imgSrc: ReproductionIcon,
+      "انجام روش IUI برای افرادی که مشکلات نازائی دارند یا اینکه می خواهند جنسیت فرزندشان را ...",
+    imgSrc: IUI,
   },
 ];
 
@@ -59,12 +59,14 @@ const LabServices = () => {
         <div className={classes.container}>
           <div className={classes.SectionHeaderContainer}>
             <div className={classes.SectionHeader}>
-              <h3>خدمات ویژه آزمایشگاه</h3>
+              <div className={classes.strong}>
+                <strong>خدمات ویژه</strong>
+              </div>
+
               <div className={classes.line}>
-                <div></div>
                 <p>
-                  آزمایشگاه نوین علاوه بر تست های معمول آزمایشگاهی ،تست ها و
-                  خدمات خاص و ویژه ای نیز ارائه می دهد :
+                  آزمایشگاه نوین علاوه بر تست های معمول آزمایشگاهی تست ها و
+                  خدمات خاص و ویژه ای نیز ارائه می دهد
                 </p>
               </div>
             </div>
@@ -75,6 +77,7 @@ const LabServices = () => {
                 return (
                   <ServiceBox
                     key={val.id}
+                    boxID={val.id}
                     title={val.title}
                     description={val.description}
                     imgSource={val.imgSrc}
