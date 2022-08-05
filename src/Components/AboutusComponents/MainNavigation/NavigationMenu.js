0fc3d13a9147f462236b30a68dd3128f805a8e7c
Ali Geshani <Triton.halley@outlook.com>
2022-08-05 18:07:19 +0430

    make Navigation menu (this is different from Main Navigation) Responsive for devices smaller than 995px

--- src/Components/AboutusComponents/MainNavigation/NavigationMenu.js
+++ src/Components/AboutusComponents/MainNavigation/NavigationMenu.js
@@ -2,14 +2,18 @@ import React, { Component, createRef } from "react";
 import classes from "./NavigationMenu.module.css";
 import logo from "../../../assets/Novin-logo.png";
 import { NavLink } from "react-router-dom";
+import menu from "../../../assets/icons/menu128-black.png";
+import closeMenu from "../../../assets/icons/close64-white.png";
 
 class NavigationMenu1 extends Component {
   constructor() {
     super();
     this.myRef = createRef();
     this.headerClasses = [classes.header, classes.posAbsolute];
+    this.displayMenuClasses = [classes.container];
     this.state = {
       headerClassesArray: this.headerClasses,
+      displayMenu: this.displayMenuClasses,
     };
   }
   componentDidMount() {
@@ -21,7 +25,6 @@ class NavigationMenu1 extends Component {
             headerClassesArray: [classes.header, classes.posSticky],
           });
         } else {
-          console.log("see");
           this.setState({
             headerClassesArray: [classes.header, classes.posAbsolute],
           });
@@ -34,74 +37,98 @@ class NavigationMenu1 extends Component {
       }
     );
     observer.observe(this.myRef.current);
-
-    // console.log(this.myRef);
   }
-
+  OpenMenuHandler() {
+    console.log(this);
+    this.setState({
+      displayMenu: [classes.container, classes.displayMenu],
+    });
+  }
+  CloseMenuHandler() {
+    this.setState({
+      displayMenu: [classes.container],
+    });
+  }
   render() {
     return (
       <>
         <div ref={this.myRef} className={classes.navigationContainer}>
           <header className={this.state.headerClassesArray.join(" ")}>
-            <div className={classes.menu}>
-              <nav className={classes.nav}>
-                <ul>
-                  <li>
-                    <NavLink
-                      className={(NavData) =>
-                        NavData.isActive ? classes.active : ""
-                      }
-                      to="/survey"
-                    >
-                      نظرسنجی
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className={(NavData) =>
-                        NavData.isActive ? classes.active : ""
-                      }
-                      to="/contact-us"
-                    >
-                      تماس با ما
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className={(NavData) =>
-                        NavData.isActive ? classes.active : ""
-                      }
-                      to="/faq"
-                    >
-                      سوالات متداول
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className={(NavData) =>
-                        NavData.isActive ? classes.active : ""
-                      }
-                      to="/about-us"
-                    >
-                      درباره ما
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className={(NavData) =>
-                        NavData.isActive ? classes.active : ""
-                      }
-                      to="/"
-                    >
-                      خانه
-                    </NavLink>
-                  </li>
-                </ul>
-              </nav>
-              <div className={classes.logo}>
-                <img src={logo} alt="آیکون آزمایشگاه نوین" />
+            <div className={this.state.displayMenu.join(" ")}>
+              <div className={classes.menu}>
+                <nav className={classes.nav}>
+                  <ul>
+                    <li>
+                      <NavLink
+                        className={(NavData) =>
+                          NavData.isActive ? classes.active : ""
+                        }
+                        to="/survey"
+                      >
+                        نظرسنجی
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={(NavData) =>
+                          NavData.isActive ? classes.active : ""
+                        }
+                        to="/contact-us"
+                      >
+                        تماس با ما
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={(NavData) =>
+                          NavData.isActive ? classes.active : ""
+                        }
+                        to="/faq"
+                      >
+                        سوالات متداول
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={(NavData) =>
+                          NavData.isActive ? classes.active : ""
+                        }
+                        to="/about-us"
+                      >
+                        درباره ما
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        className={(NavData) =>
+                          NavData.isActive ? classes.active : ""
+                        }
+                        to="/"
+                      >
+                        خانه
+                      </NavLink>
+                    </li>
+                  </ul>
+                </nav>
+                <div className={classes.logo}>
+                  <img src={logo} alt="آیکون آزمایشگاه نوین" />
+                </div>
+                <div className={classes.menuButtons}>
+                  <button
+                    onClick={this.CloseMenuHandler.bind(this)}
+                    className={classes.closeMenuBtn}
+                  >
+                    <img src={closeMenu} alt="Navbar closeMenu button" />
+                  </button>
+                </div>
               </div>
             </div>
+            <button
+              onClick={this.OpenMenuHandler.bind(this)}
+              className={classes.openMenuBtn}
+            >
+              <img src={menu} alt="Navbar menu button" />
+            </button>
           </header>
         </div>
       </>
