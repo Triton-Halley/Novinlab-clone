4908ac126430061eceb0972ba85b0a18ef47bc93
Ali Geshani <Triton.halley@outlook.com>
2022-08-06 21:24:50 +0430

    remove unnessery files

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,7 +2,6 @@ import Hero from "../Components/AboutusComponents/Hero/Hero";
 import Footer from "../Components/Footer/Footer";
 import AboutUsSection from "../Components/AboutusComponents/Aboutus/AboutusSection";
 import DoctorsSlider from "../Components/AboutusComponents/Docotors/DoctorsSlider";
-import LatestPosts from "../Components/HomePageComponents/LatestPosts/LatestPostsContainer";
 const AboutUs = () => {
   return (
     <>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,7 @@ import MainNavigation from "../Components/HomePageComponents/MainNavigation/Main
 import HeroSlider from "../Components/HomePageComponents/Hero/HeroSilder";
 import BoxContent from "../Components/HomePageComponents/Hero/HeroContentBox/Boxs";
 import LabServices from "../Components/HomePageComponents/ServicesBox/LabServices";
-import LabSection from "../Components/HomePageComponents/LabSection/LabSection";
 import ThumbnailSlider from "../Components/ThumbnailSlider/ThumbnailSlider";
-import LatestPosts from "../Components/HomePageComponents/LatestPosts/LatestPostsContainer";
 import Footer from "../Components/Footer/Footer";
 const Home = () => {
   return (
