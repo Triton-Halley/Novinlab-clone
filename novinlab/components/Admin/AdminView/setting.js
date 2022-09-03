5196fbc8cae6213775d6113d5a922303019ea515
Ali <Triton.halley@outlook.com>
2022-09-03 17:01:43 +0430

    add dashboard, users and setting view component

---
+++ novinlab/components/Admin/AdminView/setting.js
@@ -0,0 +1,13 @@
+const SettingView = () => {
+  return (
+    <>
+      <div className=" flex justify-center items-center h-screen ">
+        <div className=" flex justify-center items-center shadow-xl shadow-slate-500 w-1/2 h-1/2 rounded-2xl">
+          <h1 className="text-3xl font-bold"> Setting TODO</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SettingView;
---
+++ novinlab/components/Admin/AdminView/users.js
@@ -0,0 +1,13 @@
+const UsersView = () => {
+  return (
+    <>
+      <div className=" flex justify-center items-center h-screen ">
+        <div className=" flex justify-center items-center shadow-xl shadow-slate-500 w-1/2 h-1/2 rounded-2xl">
+          <h1 className="text-3xl font-bold"> Users TODO</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UsersView;
