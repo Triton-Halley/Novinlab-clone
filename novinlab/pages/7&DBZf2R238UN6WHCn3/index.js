f64ac876e0cb5667768a5580efca772766891547
Ali <Triton.halley@outlook.com>
2022-09-03 16:58:49 +0430

    check user is Auth
    if  user didn't Authenticate redirect him to login page

--- novinlab/pages/7&DBZf2R238UN6WHCn3/index.js
+++ novinlab/pages/7&DBZf2R238UN6WHCn3/index.js
@@ -1,13 +1,16 @@
 /*** Admin Panel Route */
 import { getSession } from "next-auth/react";
 import Head from "next/head";
+import Panel from "../../components/Admin/Panel";
 const AdminPanel = () => {
   return (
     <>
       <Head>
         <title>داشبورد</title>
       </Head>
-      <div>Admin panel</div>
+      <div className="overflow-hidden">
+        <Panel />
+      </div>
     </>
   );
 };
@@ -16,10 +19,9 @@ export default AdminPanel;
 
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
-
+  console.log("ththhththt", session);
   if (!session) {
     return {
-      notFound: true,
       redirect: {
         // login page path
         destination: "/ueCQUto1TR8Hzl63iye9SP",
