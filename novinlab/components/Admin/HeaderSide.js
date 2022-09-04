10f561c817c4fbb5e5074b1d7cff01df6b8998be
Ali <Triton.halley@outlook.com>
2022-09-04 10:12:57 +0430

    add signout function

--- novinlab/components/Admin/HeaderSide.js
+++ novinlab/components/Admin/HeaderSide.js
@@ -1,10 +1,14 @@
 import { MenuRounded, LogoutRounded } from "@mui/icons-material";
+import { signOut } from "next-auth/react";
 const HeaderSide = () => {
   return (
     <div className="flex justify-between w-full py-6 px-4 shadow-lg">
       <div className="flex space-x-10 items-center">
         <div>
-          <button className="cursor-pointer flex items-center space-x-4 text-xl bg-slate-300 p-4 rounded-md">
+          <button
+            onClick={(e) => signOut()}
+            className="cursor-pointer flex items-center space-x-4 text-xl bg-slate-300 p-4 rounded-md"
+          >
             <LogoutRounded className="text-2xl" />
             <span>خروج</span>
           </button>
