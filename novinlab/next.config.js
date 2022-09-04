d9e787ac88b65f84f8d0c389d73e77a45a6d68e6
Ali <Triton.halley@outlook.com>
2022-09-04 10:12:07 +0430

    add material ui config to next.comfig.js

--- novinlab/next.config.js
+++ novinlab/next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  modularizeImports: {
+    "@gorazdo/tomui": {
+      transform: "@gorazdo/tomui/{{member}}",
+    },
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/icons-material/?(((\\w*)?/?)*)": {
+      transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
